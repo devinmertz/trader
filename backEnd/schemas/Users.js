e7c71a4e028d8c5dd8d47467e20b055b1cdb4b26
shanm19 <shanm19@gmail.com>
2016-10-13 14:05:09 -0600

    add token auth

--- backEnd/schemas/Users.js
+++ backEnd/schemas/Users.js
@@ -1,8 +1,10 @@
 var mongoose = require('mongoose');
-var Schema = mongoose.schema;
+var Schema = mongoose.Schema;
 var ItemSchema = require('./Items').Schema;
+//Token Auth
+var bcrypt = require('bcrypt');
 
-var UserSchema = new mongoose.Schema({
+var userSchema = new Schema({
 	firstName: {
 		type: String,
 		required: true
@@ -11,7 +13,12 @@ var UserSchema = new mongoose.Schema({
 		type: String,
 		required: true
 	},
-	userName: {
+	username: {
+		type: String,
+		required: true, 
+		unique: true
+	},
+	password: {
 		type: String,
 		required: true
 	},
@@ -35,4 +42,30 @@ var UserSchema = new mongoose.Schema({
 
 });
 
-module.exports = mongoose.model('User', UserSchema);
+// Token Auth password hash
+userSchema.pre("save", function (next) {
+	var user = this;
+	
+	if (!user.isModified("password")) next();
+	
+	bcrypt.hash(user.password, 10, function (err, hash) {
+		if (err) console.log(err);
+		user.password = hash;
+		next();
+	});
+});
+
+userSchema.methods.comparePasswords = function (passwordAttempt, callback) {
+	bcrypt.compare(passwordAttempt, this.password, function (err, isMatch) {
+		if (err) callback(err);
+		callback(null, isMatch);
+	});
+};
+
+userSchema.methods.removePassword = function () {
+	var user = this.toObject();
+	delete user.password;
+	return user;
+};
+
+module.exports = mongoose.model('User', userSchema);
--- backEnd/server.js
+++ backEnd/server.js
@@ -4,30 +4,41 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 var path = require('path');
 var app = express();
-
-var port = process.env.PORT || 8080;
-
+var config = require('./config');
 var mongoose = require('mongoose');
+var jwt = require('express-jwt');
+
 var Items = require('./schemas/Items');
 var Users = require('./schemas/Users');
 
-mongoose.connect('mongodb://localhost/trader');
-
-
-
 var userRoute = require('./routes/userRoute');
 var itemRoute = require('./routes/itemRoute');
+var authRoute = require('./routes/authRoute');
 //var routes = require('./routes/index');
+
+var port = process.env.PORT || 8080;
+
+mongoose.connect(config.database, function(){
+    console.log("Mongoose on the DB");
+});
 
 app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 //app.use('/', routes);
 app.use('/user', userRoute);
 app.use('/item', itemRoute);
+app.use('/auth', authRoute);
 
-app.use(express.static(path.join(__dirname, "../frontEnd/")));
+app.use('api', jwt({
+    secret: config.secret
+}));
+
+app.use(express.static(path.join(__dirname, "frontEnd")));
 
 app.listen(port, function () {
     console.log("You have reached port " + port)
